484153a3f939095143f994d631961178ebb6c41d
Moroz Tetiana <tania@Morozs-MacBook-Air.local>
2023-05-04 19:24:50 +0300

    sorted students by name, surname, married, age

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,74 @@
 
 export interface Student {
-  // describe Student interface
+  name: string;
+  surname : string;
+  age: number;
+  married: boolean;
+  grades: number[];
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name = 'name',
+  Surname = 'surname',
+  Age = 'age',
+  Married = 'married',
+  AverageGrade = 'averageGrade'
+
 }
 
 // create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export const sortStudents = (
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder,
+): Student[] => {
+  const arrayStudents = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
-}
+  arrayStudents.sort((
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    a: Student & Record<string, any>,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    b: Student & Record<string, any>,
+  ) => {
+    const keyA = a?.[sortBy];
+    const keyB = b?.[sortBy];
+
+    if (typeof keyA === 'string' && typeof keyB === 'string') {
+      return order === 'asc' ? keyA.localeCompare(keyB)
+        : keyB.localeCompare(keyA);
+    }
+
+    if (typeof keyA === 'object') {
+      const totalGradeA = (keyA as number[]).reduce(
+        // eslint-disable-next-line no-param-reassign
+        (acc, curr) => (acc += curr), 0,
+      ) / (keyA as number[]).length;
+
+      const totalGradeB = (keyB as number[]).reduce(
+        // eslint-disable-next-line no-param-reassign
+        (acc, curr) => (acc += curr), 0,
+      ) / (keyB as number[]).length;
+
+      return order === 'asc' ? totalGradeA - totalGradeB
+        : totalGradeB - totalGradeA;
+    }
+
+    if (typeof keyA === 'boolean' && typeof keyB === 'boolean') {
+      if (!keyA && keyB) {
+        return 1;
+      }
+
+      if (keyA && !keyB) {
+        return -1;
+      }
+
+      return 0;
+    }
+
+    return order === 'asc' ? keyA - keyB : keyB - keyA;
+  });
+
+  return arrayStudents;
+};
